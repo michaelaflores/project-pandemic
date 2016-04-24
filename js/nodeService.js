99a72294b6ef6c8b65655d6e97643c4e5af26a24
michaelaflores <floresm2@miamioh.edu>
2016-04-23 23:31:19 -0400

    initial commit
    Mostly sketching out the function, work in progress for createNoder()

---
+++ js/nodeService.js
@@ -0,0 +1,45 @@
+pandemic.factory('nodeService', nodeService);
+
+function nodeService($log) {
+  return {
+    createNodes: createNodes,
+    deleteNode: deleteNode,
+    updateNode: updateNode
+  }
+
+  function createNodes(numberToCreate, initialInfectedCount, averageInfectionFactor,
+    averageResistanceFactor, averageInfectedSpeed, averageNormalSpeed) {
+    var nodesArray = [];
+    var infectedCreated = 0;
+    for (var num = 0, num <= numberToCreate, num++) {
+      var status;
+      if (infectedCreated < initialInfectedCount) {
+        status = 1;
+      } else {
+        status = 0;
+      }
+      var randomSpeed = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+      var randomFactor = ;
+      nodesArray.push(new Node(status, randomSpeed, randomFactor));
+    }
+
+    // placeholder
+    function deleteNode() {
+      console.log('deleteNode()');
+    }
+
+    // placeholder
+    function updateNode() {
+      console.log('updateNode()');
+    }
+
+    // constructor function for Node objects
+    function Node(status, speed, factor) {
+      this.status = status;
+      this.speed = speed;
+      this.factor;
+      this.x;
+      this.y;
+    }
+  }
+}
